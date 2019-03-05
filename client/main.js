c73870b63ce2a2bf13474087f18774c5d2971ab5
vvkharivallabha <vvkharivallabha09@gmail.com>
2019-03-05 19:59:16 +0530

    app.js

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { hydrate } from 'react-dom'
-import App from './App'
+import React from "react";
+import { hydrate } from "react-dom";
+import App from "./App.js";
 
-hydrate(<App/>, document.getElementById('root'))
+hydrate(<App />, document.getElementById("root"));
