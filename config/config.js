86efe7252a365c5d776fba31983398920bff5fe8
vvkharivallabha <vvkharivallabha09@gmail.com>
2019-03-05 20:36:43 +0530

    aqwfedw

--- config/config.js
+++ config/config.js
@@ -1,12 +1,9 @@
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
-}
+  mongoUri:
+    "mongodb://Vallabha:V@llabha1@cluster0-shard-00-00-fpqvq.mongodb.net:27017,cluster0-shard-00-01-fpqvq.mongodb.net:27017,cluster0-shard-00-02-fpqvq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+};
 
-export default config
+export default config;
